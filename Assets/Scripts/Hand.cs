bad2b8829f8eb7d305c5dad5455bbfc1ca9b4957
RChygir <30907647+RChygir@users.noreply.github.com>
2020-01-04 22:13:33 +0200

    Add Hand prototype

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    private const int HAND_SIZE = 4;
+    private int card_in_hand = 0;
+
+    private List<Card> player_hand = new List<Card>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetFullHand()
+    {
+
+    }
+
+    public void GetCard()
+    {
+        
+    }
+
+    public void PlayCard()
+    {
+
+    }
+
+    public void ChangeCard()
+    {
+
+    }
+}
